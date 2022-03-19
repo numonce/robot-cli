9d720ab27ca7d3564cd045c8288c2b7aead88d6f
numonce <numonce@protonmail.com>
2022-03-19 18:18:35 +0000

    Added support for udp, better error handeling, and now uses clap for arguments.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
+use clap::{Arg, Command};
 use crossterm::event::{read, Event, KeyModifiers};
-use crossterm::style::Stylize;
 use crossterm::style::*;
 use crossterm::ExecutableCommand;
-use std::env;
 use std::io::{stdout, Write};
-use std::net::TcpStream;
+use std::net::{TcpStream,UdpSocket};
 
-fn init() -> crossterm::Result<()> {
+fn terminal() -> crossterm::Result<()> {
     let mut stdout = stdout();
     stdout.execute(crossterm::terminal::Clear(
         crossterm::terminal::ClearType::All,
@@ -93,19 +92,10 @@ fn init() -> crossterm::Result<()> {
     Ok(())
 }
 
-
-fn connection() -> Result<(), Box<dyn std::error::Error>>{
-
-    for argument in env::args() {
-        if argument == "--help" || argument == "-h" {
-            println!("./robot-cli <ip> <port>");
-        }
-    }
-
-    let args: Vec<String> = env::args().collect();
-    let robot = format!("{}:9999", args[1]);
-    let mut stream = TcpStream::connect(&robot)?;
-
+fn tcp_connection(ip: &str, port: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let target = format!("{}:{}", ip.to_string(), port.to_string());
+    let mut stream = TcpStream::connect(&target)?;
+    terminal()?;
     let q = crossterm::event::KeyCode::Char('q');
     let w = crossterm::event::KeyCode::Char('w');
     let a = crossterm::event::KeyCode::Char('a');
@@ -141,19 +131,100 @@ fn connection() -> Result<(), Box<dyn std::error::Error>>{
     Ok(())
 }
 
-
-fn main() -> Result<(),Box<dyn std::error::Error>> {
-    init()?;
-    crossterm::terminal::enable_raw_mode()?;
-    let res = connection();
-    if let Err(e) = res {
-    eprintln!("{:?}",e);
-    std::thread::sleep(std::time::Duration::from_secs(3));
+fn udp_connection(ip: &str, port: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let socket = UdpSocket::bind("127.0.0.1:12345")?;
+    let target = format!("{}:{}", ip.to_string(), port.to_string());
+    socket.connect(target)?;
+    terminal()?;
+    let q = crossterm::event::KeyCode::Char('q');
+    let w = crossterm::event::KeyCode::Char('w');
+    let a = crossterm::event::KeyCode::Char('a');
+    let s = crossterm::event::KeyCode::Char('s');
+    let d = crossterm::event::KeyCode::Char('d');
+    let quit = crossterm::event::KeyEvent::new(q, KeyModifiers::NONE);
+    let foward = crossterm::event::KeyEvent::new(w, KeyModifiers::NONE);
+    let left = crossterm::event::KeyEvent::new(a, KeyModifiers::NONE);
+    let back = crossterm::event::KeyEvent::new(s, KeyModifiers::NONE);
+    let right = crossterm::event::KeyEvent::new(d, KeyModifiers::NONE);
+    loop {
+        let keystroke = read()?;
+        if keystroke == Event::Key(quit) {
+            break;
+        } else if keystroke == Event::Key(foward) {
+            let buf = "1\n".as_bytes();
+            socket.send(buf)?;
+        } else if keystroke == Event::Key(left) {
+            let buf = "2\n".as_bytes();
+            socket.send(buf)?;
+        } else if keystroke == Event::Key(back) {
+            let buf = "3\n".as_bytes();
+            socket.send(buf)?;
+        } else if keystroke == Event::Key(right) {
+            let buf = "4\n".as_bytes();
+            socket.send(buf)?;
+        }
+        
     }
-    crossterm::terminal::disable_raw_mode()?;
-    stdout().execute(ResetColor)?;
-    stdout().execute(crossterm::terminal::Clear(
-        crossterm::terminal::ClearType::All,
-    ))?;
+    Ok(())
+}
+
+fn init() -> Result<(), Box<dyn std::error::Error>> {
+    let args = Command::new("robot-cli")
+        .version("1.0.1")
+        .author("numonce")
+        .about("A simple rust program used to control robots")
+        .arg(
+            Arg::new("IP")
+                .long("ip")
+                .short('i')
+                .takes_value(true)
+                .required(true)
+                .help("IP of the target robot."),
+        )
+        .arg(
+            Arg::new("port")
+                .long("port")
+                .short('p')
+                .takes_value(true)
+                .required(true)
+                .help("Port of the target robot"),
+        )
+        .arg(
+            Arg::new("udp")
+                .long("udp")
+                .short('u')
+                .takes_value(false)
+                .help("Specifies a UDP connection."),
+        )
+        .get_matches();
+
+    let ip = args.value_of("IP").unwrap();
+    let port = args.value_of("port").unwrap();
+    crossterm::terminal::enable_raw_mode()?;
+    if args.is_present("udp") {
+        udp_connection(ip, port)?;
+    } else {
+        tcp_connection(ip, port)?;
+    }
+
+    Ok(())
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let res = init();
+    if let Err(e) = res {
+        crossterm::terminal::disable_raw_mode()?;
+        stdout().execute(ResetColor)?;
+        stdout().execute(crossterm::terminal::Clear(
+            crossterm::terminal::ClearType::All,
+        ))?;
+        eprintln!("{:?}", e);
+    } else {
+        crossterm::terminal::disable_raw_mode()?;
+        stdout().execute(ResetColor)?;
+        stdout().execute(crossterm::terminal::Clear(
+            crossterm::terminal::ClearType::All,
+        ))?;
+    }
     Ok(())
 }
